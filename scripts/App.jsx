a096b51ea6bb489c3755ccc49287f1d86f32190a
aswinbhaskaran <66071969+aswinbhaskaran@users.noreply.github.com>
2024-06-07 10:35:27 +0000

    Files modified

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,7 +1,8 @@
 import Aside from './Aside'
 import Section from './Section'
-import '../styles/style.css'
 import { useState } from 'react'
+
+CSS.supports('selector(&)') ? import('../styles/nesting-style.css') : import('../styles/non-nesting-style.css')
 
 const els = [{id: 1, h1: 'Personal info', p: 'Please provide your name, email address, and phone number.', eID: 'personal-form'},
 	{id: 2, h1: 'Select your plan', p: 'You have the option of monthly or yearly billing', eID: 'plan-form'}]
@@ -12,12 +13,7 @@ function App()
 
 	return (<>
 			<Aside />
-			<Section
-				els={els}
-				id={elementID}
-				prev={() => setElementID(elementID - 1)}
-				next={() => setElementID(elementID + 1)}
-			/>
+			<Section els={els} id={elementID} prev={() => setElementID(elementID - 1)} next={() => setElementID(elementID + 1)}/>
 		</>)
 }
 
