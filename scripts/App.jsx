384908a77da750ea9a684edd0edaab98cc24c389
aswinbhaskaran <bhaskaranaswin1994@gmail.com>
2024-06-08 16:06:28 +0530

    Scripts updated

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -11,23 +11,37 @@ const els = [{id: 1, h1: 'Personal info', p: 'Please provide your name, email ad
 function App()
 {
 	const [elementID, setElementID] = useState(1)
-
-	function prevPage(e)
-	{
-		console.info(e.target.parentElement.children[1].id)
-		setElementID(elementID - 1)
-	}
 	
 	function nextPage(e)
 	{
-		console.info(e.target.parentElement.children[1].id)
-		setElementID(elementID + 1)
+		const elem = e.target.parentElement.children[1]
+
+		if(elem.id == 'personal-form')
+		{
+			const em = elem.querySelector('input[type=email]')
+
+			for(const el of elem.querySelectorAll('input[type=text]'))
+				if(el.value == '')
+				{
+					el.classList.add('invalid')
+
+					el.parentElement.children[0].dataset.invalid = 'This field is required'
+				}
+
+			if(em.validity.valid && (em.value != ''))
+				setElementID(elementID + 1)
+			else
+			{
+				em.parentElement.children[0].dataset.invalid = 'This field is required'
+				em.classList.add('invalid')
+			}
+		}
 	}
 
 	return (<>
 			<Aside cur={elementID} els={els}/>
 			
-			<Section els={els} id={elementID} prev={prevPage} next={nextPage}/>
+			<Section els={els} id={elementID} prev={() => setElementID(elementID - 1)} next={nextPage}/>
 		</>)
 }
 
--- scripts/Personal.jsx
+++ scripts/Personal.jsx
@@ -1,5 +1,30 @@
 function Personal({h1, p, id, next})
 {
+	function focusIn(e)
+	{
+		const el = e.target
+
+		if(el.classList.contains('invalid') && (el.classList.length == 1))
+		{
+			el.removeAttribute('class')
+
+			el.parentElement.children[0].dataset.invalid = ''
+		}
+	}
+
+	function focusOut(e)
+	{
+		const el = e.target
+
+		if(el.type == 'email')
+			if(!el.validity.valid)
+			{
+				el.classList.add('invalid')
+
+				el.parentElement.children[0].dataset.invalid = 'This field is required'
+			}
+	}
+
 	return (<>
 			<header>
 				<h1>{h1}</h1>
@@ -11,19 +36,19 @@ function Personal({h1, p, id, next})
 				<div>
 					<label htmlFor="name" data-label="Name"></label>
 
-					<input type="text" placeholder="e.g. Stephen King" id="name"/>
+					<input type="text" placeholder="e.g. Stephen King" id="name" onFocus={focusIn}/>
 				</div>
 
 				<div>
 					<label htmlFor="email" data-label="Email Address"></label>
 
-					<input type="email" placeholder="e.g. stephenking@lorem.com" id="email"/>
+					<input type="email" placeholder="e.g. stephenking@lorem.com" id="email" onFocus={focusIn} onBlur={focusOut}/>
 				</div>
 
 				<div>
 					<label htmlFor="ph" data-label="Phone Number"></label>
 
-					<input type="text" placeholder="e.g. +1 234 567 890" id="ph"/>
+					<input type="text" placeholder="e.g. +1 234 567 890" id="ph" onFocus={focusIn}/>
 				</div>
 			</div>
 
