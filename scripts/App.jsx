80e80228a33bacb8d31ebdec8697f5befc1169e8
aswinbhaskaran <bhaskaranaswin1994@gmail.com>
2024-06-10 14:12:53 +0530

    Files updated

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,9 +1,7 @@
 import Aside from './Aside'
 import Section from './Section'
 import { useState } from 'react'
-// import '../styles/nesting-style.css'
-
-CSS.supports('selector(&)') ? import('../styles/nesting-style.css') : import('../styles/non-nesting-style.css')
+import '../styles/nesting-style.css'
 
 const els = [{id: 1, h1: 'Personal info', p: 'Please provide your name, email address, and phone number.', eID: 'personal-form', s1: 'Step 1', s2: 'Your info'},
 	{id: 2, h1: 'Select your plan', p: 'You have the option of monthly or yearly billing', eID: 'plan-form', s1: 'Step 2', s2: 'Select plan'}]
