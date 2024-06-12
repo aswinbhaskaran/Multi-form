696fb512578aaa03e690327a014eac2efb449613
aswinbhaskaran <66071969+aswinbhaskaran@users.noreply.github.com>
2024-06-12 07:29:38 +0000

    Files updated

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,14 +1,14 @@
 import Aside from './Aside'
 import Section from './Section'
 import { useState } from 'react'
-import '../styles/nesting-style.css'
+import '../styles/non-nesting-style.css'
 
 const els = [{id: 1, h1: 'Personal info', p: 'Please provide your name, email address, and phone number.', eID: 'personal-form', s1: 'Step 1', s2: 'Your info'},
 	{id: 2, h1: 'Select your plan', p: 'You have the option of monthly or yearly billing', eID: 'plan-form', s1: 'Step 2', s2: 'Select plan'}]
 
 function App()
 {
-	const [elementID, setElementID] = useState(1)
+	const [elementID, setElementID] = useState(2)
 	
 	function nextPage(e)
 	{
